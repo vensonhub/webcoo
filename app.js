544e00779228ab0a1d630a57b295f6f2ac2aa0c0
vensonhub <283747923@qq.com>
2017-07-17 12:24:02 +0800

    fix web

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('app is running');
+const http = require('http');
+
+http.createServer(function(req,res){
+        res.writeHead(200,{'Content-Type':'text/plain'})
+        res.end('mooc website')
+}).listen(8080,()=>{
+        console.log("mooc  server running");
+})
